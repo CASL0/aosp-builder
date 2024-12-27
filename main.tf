8c96dca8accd0318c5d452b4d2e2782437af2afd
CASL0 <gin.0046.822@gmail.com>
2024-12-28 01:09:47 +0900

    feat: タグ修正

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+locals {
+  tags = {
+    Name      = "AOSP"
+    Terraform = true
+  }
+}
+
 module "aws_resources" {
   source = "./modules/aws"
+
+  ec2_key_pair = "ec2-key"
+
+  tags = local.tags
 }
